a16f53750d59826ce465ad40f51b0bea3d5cf26b
unknown <maxim.tickhonovich@gmail.com>
2018-08-22 14:12:06 +0300

    add WindowGameF

--- WindowGameF/Form1.cs
+++ WindowGameF/Form1.cs
@@ -7,14 +7,60 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BoardF;
 
 namespace WindowGameF
 {
-    public partial class Form1 : Form
+    public partial class FormName15 : Form
     {
-        public Form1()
+        const int size = 4;
+        Game game;
+        public FormName15()
         {
             InitializeComponent();
+            game = new Game(size);
+            HideButtons();
+        }
+
+        private void b00_Click(object sender, EventArgs e)
+        {
+            if (game.Solved())
+                return;
+            Button button = (Button)sender; //b00
+            int x = int.Parse(button.Name.Substring(1, 1));
+            int y = int.Parse(button.Name.Substring(2, 1));
+            game.PressAt(x, y);
+            ShowButtons();
+
+            if (game.Solved())
+                labelMoves.Text = "Game finished in " + game.moves + " moves";
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            game.Start (1000 + DateTime.Now.DayOfYear);
+            ShowButtons();
+        }
+
+        void HideButtons() //скрытие кнопок
+        {
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    ShowDigitAt(0, x, y);
+        }
+        void ShowButtons()
+        {
+            for (int x = 0; x < size; x++)
+                for (int y = 0; y < size; y++)
+                    ShowDigitAt(game.GetDigitAt(x, y), x, y);
+            labelMoves.Text = game.moves.ToString() + " moves";
+        }
+
+        void ShowDigitAt(int digit, int x, int y)
+        {
+            Button button = (Button)Controls["b" + x + y];
+            button.Text = digit.ToString();
+            button.Visible = digit > 0;
         }
     }
 }
--- WindowGameF/Program.cs
+++ WindowGameF/Program.cs
@@ -16,7 +16,7 @@ namespace WindowGameF
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormName15());
         }
     }
 }
